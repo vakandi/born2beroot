24edb60004e184bb631c8baa19b0aede873626ec
vakandi <wael.bousfira@protonmail.com>
2022-11-25 17:01:54 -0500

    update

---
+++ correction.sh
@@ -0,0 +1,16 @@
+dpkg -i libglapi-mesa_20.3.5-1_amd64.deb
+dpkg -i libglvnd0_1.3.2-1_amd64.deb
+dpkg -i libglx0_1.3.2-1_amd64.deb
+dpkg -i libdbus-1-3_1.12.20-2_amd64.deb
+dpkg -i dbus-x11_1.12.20-2_amd64.deb
+dpkg -i *.deb
+aptmaj
+apt install dbus
+apt install dbus-daemon dbus-user-session dbus-x11 
+aptmaj
+apt install xorg
+apt install libglvnd0 libegl-mesa0 libglapi-mesa
+apt install libgck-1-0 libgcr-base-3-1 gcr p11-kit libdbus-1-3 
+apt install dbus-daemon dbus-user-session dbus-x11 
+aptmaj
+apt install gdm3
