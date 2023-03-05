2395c3d85dbf1246fa217adacf6c1a18a78eedcf
vakandi <wael.bousfira@protonmail.com>
2023-03-05 00:03:17 -0500

    update

--- full_live_bash_monitoring.sh
+++ full_live_bash_monitoring.sh
@@ -15,8 +15,9 @@ TOTAL_DISK_GIGA=$(echo "$(df -BG | awk '{print $2}' | sed '1d' | rev | cut -c2-|
 FREE_DISK_GIGA=$( echo "$(df -BG | awk '{print $4}' | sed '1d' | rev | cut -c2-| rev  )" > giga.tmp && awk '{s+=$1} END {print s}' giga.tmp)
 REAL_FREE_DISK_GIGA=$( echo "$TOTAL_DISK_GIGA - $FREE_DISK_GIGA" |bc)    
 PERCENT_GIGA=$(echo "(100 - (100*$FREE_DISK_GIGA/$TOTAL_DISK_GIGA))" |bc)
-LVM_CHECK="$(sudo lvscan | awk '{print $1}' | sed '1,6d')"
+
 NUM_TCP=$(ss -s | grep TCP | sed '1d' | cut -c7)
+
 TOR_CHECK=$(curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ | cat | grep -m 1 Congratulations | xargs)
 CPU_LOAD9="$(mpstat | awk '{print $3}' | sed '1,3d'| tr "," ".")"
 CPU_LOAD1="$(mpstat | awk '{print $4}' | sed '1,3d'| tr "," ".")"
@@ -46,14 +47,30 @@ echo -e "${BLUE}CPU Load : ${END}${GREEN}$RESULT_CPU_LOAD %${END}"
 
 echo -e "${BLUE}Last Boot:${END}${GREEN} $(last reboot |less| head -n1 | cut -c40-56)${END}"
 
-#if [ $LVM_CHECK = "ACTIVE" ]
-#then
-if [ -n "$LVM_CHECK" ] && [ "$LVM_CHECK" = "ACTIVE" ]; then
-	echo -e "${BLUE}LVM use: ${END}${GREEN}yes${END} "
-else
-	echo -e "${BLUE}LVM use: ${END}${GREEN}no${END} "
+lvm_enabled=0
+
+# Check if the lvm2 package is installed
+if dpkg-query -W lvm2 > /dev/null 2>&1; then
+    # Check if the dm-mod kernel module is loaded
+    if lsmod | grep -q "^dm_mod "; then
+        lvm_enabled=1
+    fi
 fi
-NUM_TCP=$(ss -s | grep TCP | sed '1d' | cut -c7)
+
+if [[ $lvm_enabled -eq 1 ]]; then
+    echo -e "${BLUE}LVM use: ${END}${GREEN}yes${END}"
+else
+    echo -e "${BLUE}LVM use: ${END}${GREEN}no${END}"
+fi
+
+#This script checks if LVM is enabled on the system by checking two conditions:
+
+#It checks if the lvm2 package is installed on the system. LVM relies on this package to provide the dmsetup tool, which is used to manage LVM logical volumes.
+#It checks if the dm-mod kernel module is loaded. LVM uses the device mapper kernel subsystem to manage logical volumes, and the dm-mod module is necessary for this subsystem to function.
+#If both of these conditions are met, the script outputs "LVM is enabled on this system." Otherwise, it outputs "LVM is not enabled on this system."
+
+
+
 
 echo -e "${BLUE}Connections TCP : ${END}${GREEN}$NUM_TCP${END} ${BLUE} ESTABLISHED${END} "
 
@@ -79,7 +96,7 @@ do
 	echo -e "${BLUE}MAC Address of the current internet connection(s):${END}\n${GREEN}$MAC_ADDRESS${END}"
 	
 	
-	echo -e "${BLUE}Sudo : ${END}${GREEN}$(sudo cat /var/log/sudo/sudo.log | grep COMMAND | awk '{print NR}' | awk 'END { print }') cmd${END}"
+	echo -e "${BLUE}Sudo : ${END}${GREEN}$(cat /var/log/sudo/sudo.log | grep COMMAND | awk '{print NR}' | awk 'END { print }') cmd${END}"
 	#7 output lines from my script + mac address to add
 	number_of_lines_to_clear=10
 	#Adding the mac  address to equal to one line each
